32159109098bb2be25c90bf83eeb8dc4061d3577
Lucas Almeida Petrolli <lucaslap27@gmail.com>
2025-03-29 17:45:46 -0400

    feat: cria nova classe para comando de pesquisa

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import asyncio
 from help_cog import help_cog
 from music_cog import music_cog
 from dotenv import load_dotenv
+from search_cog import search_cog
+
 load_dotenv()
 TOKEN_DISCORD=os.getenv("TOKEN_DISCORD")
 
@@ -23,6 +25,7 @@ async def main():
     async with bot:
         await bot.add_cog(help_cog(bot))
         await bot.add_cog(music_cog(bot))
+        await bot.add_cog(search_cog(bot))
         await bot.start(TOKEN_DISCORD)
 
 asyncio.run(main())
--- music_cog.py
+++ music_cog.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from youtubesearchpython import VideosSearch
 from yt_dlp import YoutubeDL
 import asyncio
-import wikipedia
+
 import subprocess
 
 
@@ -154,35 +154,3 @@ class music_cog(commands.Cog):
     async def re(self, ctx):
         self.music_queue.pop()
         await ctx.send("```last song removed```")
-
-    @commands.command(name="wiki", help="pesquise alguma coisa na wikipidia")
-    async def wiki(self, ctx: commands.Context, *, pesquisa):
-        try:
-            wikipedia.set_lang("pt")
-            limite_resumo = 1500  # Limite para o resumo
-            buscas = wikipedia.search(pesquisa)
-            if not buscas:
-                await ctx.reply("Nenhum resultado encontrado para essa pesquisa.")
-                return
-
-            pagina = wikipedia.page(buscas[0], auto_suggest=False)
-            resumo = wikipedia.summary(pagina.title,
-                                       sentences=5)  # Obter um resumo (ajuste o número de sentenças conforme necessário)
-            link = pagina.url
-
-            mensagem = f"**{pagina.title}**\n\n{resumo[:limite_resumo] + '...' if len(resumo) > limite_resumo else resumo}\n\nLeia mais: {link}"
-
-            if len(mensagem) > 2000:  # Check if the final message exceeds Discord's limit
-                partes = [mensagem[i:i + 2000] for i in range(0, len(mensagem), 2000)]
-                for parte in partes:
-                    await ctx.reply(parte)
-            else:
-                await ctx.reply(mensagem)
-
-        except wikipedia.exceptions.PageError:
-            await ctx.reply(f"Não encontrei a página '{pesquisa}' na Wikipédia em português.")
-        except wikipedia.exceptions.DisambiguationError as e:
-            await ctx.reply(
-                f"Resultado ambíguo para '{pesquisa}'. Por favor, seja mais específico.\nOpções: {', '.join(e.options[:10])}")
-        except Exception as erro:
-            await ctx.reply(f"Ocorreu um erro ao buscar na Wikipédia: {erro}")
