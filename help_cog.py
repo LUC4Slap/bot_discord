8ad2f23df3ad61fc650f8e15d1067626fb162235
Lucas Almeida Petrolli <lucaslap27@gmail.com>
2025-03-29 16:47:05 -0400

    feat: traduz comandos do bot

--- help_cog.py
+++ help_cog.py
@@ -12,17 +12,18 @@ class help_cog(commands.Cog):
     def set_message(self):
         self.help_message = f"""
             ```
-            General commands:
-            {self.bot.command_prefix}help - displays all the available commands
-            {self.bot.command_prefix}q - displays the current music queue
-            {self.bot.command_prefix}p <keywords> - finds the song on youtube and plays it in your current channel. Will resume playing the current song if it was paused
-            {self.bot.command_prefix}skip - skips the current song being played
-            {self.bot.command_prefix}clear - Stops the music and clears the queue
-            {self.bot.command_prefix}stop - Disconnected the bot from the voice channel
-            {self.bot.command_prefix}pause - pauses the current song being played or resumes if already paused
-            {self.bot.command_prefix}resume - resumes playing the current song
-            {self.bot.command_prefix}prefix - change command prefix
-            {self.bot.command_prefix}remove - removes last song from the queue
+General commands:
+{self.bot.command_prefix}help - exibe todos os comandos disponíveis
+{self.bot.command_prefix}q - exibe a fila de músicas atual
+{self.bot.command_prefix}p - encontra a música no youtube e a reproduz no seu canal atual. Retoma a reprodução da música atual se ela foi pausada
+{self.bot.command_prefix}skip - pula a música atual que está sendo tocada
+{self.bot.command_prefix}clear - Para a música e limpa a fila
+{self.bot.command_prefix}stop - Desconectou o bot do canal de voz
+{self.bot.command_prefix}pause - pausa a música atual que está sendo tocada ou retoma se já estiver pausada
+{self.bot.command_prefix}resume - retoma a reprodução da música atual
+{self.bot.command_prefix}prefix - alterar prefixo de comando
+{self.bot.command_prefix}remove - remove a última música da fila
+{self.bot.command_prefix}wiki - faz uma busca na wikipidia - ex: [!wiki brasil]
             ```
             """
 
